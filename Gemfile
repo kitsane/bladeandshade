4e9b0a40274800fab91c6e6a359351419f72c634
Andi Staub <sta@panter.ch>
2014-07-25 13:28:43 +0200

    Add middleman skeleton and assets.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'middleman'
+gem 'middleman-livereload'
+gem 'middleman-sprockets'
+gem 'middleman-gh-pages'
+
+gem 'bootstrap-sass'
+
+gem 'builder'
---
+++ config.rb
@@ -0,0 +1,84 @@
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+require 'bootstrap-sass'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'styles'
+
+set :js_dir, 'scripts'
+
+set :images_dir, 'images'
+
+# set :relative_links, true
+
+# Enable live reload while working locally
+activate :livereload
+
+# Build-specific configuration
+#
+configure :build do
+
+  # For example, change the Compass output styles for deployment
+  activate :minify_css
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :cache_buster
+
+  # Use relative URLs
+  activate :relative_assets
+  set :relative_links, true
+
+  # Compress PNGs after build
+  # activate :smusher
+
+  # Build with nice urls
+  activate :directory_indexes
+
+end
