92c3d8691283bf17b76a31dabcdca0792005c72d
Andi Staub <sta@panter.ch>
2014-08-02 14:04:24 +0200

    Add favicon builer and use it.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'middleman'
 gem 'middleman-livereload'
 gem 'middleman-sprockets'
 gem 'middleman-gh-pages'
+gem 'middleman-favicon-maker', '~> 3.7'
 
 gem 'bootstrap-sass'
 
--- config.rb
+++ config.rb
@@ -80,4 +80,26 @@ configure :build do
   # Build with nice urls
   activate :directory_indexes
 
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source', 'img', 'favicon')
+    f.icons = {
+      "_favicon.png" => [
+        { icon: "apple-touch-icon-152x152-precomposed.png" },
+        { icon: "apple-touch-icon-144x144-precomposed.png" },
+        { icon: "apple-touch-icon-120x120-precomposed.png" },
+        { icon: "apple-touch-icon-114x114-precomposed.png" },
+        { icon: "apple-touch-icon-76x76-precomposed.png" },
+        { icon: "apple-touch-icon-72x72-precomposed.png" },
+        { icon: "apple-touch-icon-60x60-precomposed.png" },
+        { icon: "apple-touch-icon-57x57-precomposed.png" },
+        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
+        { icon: "apple-touch-icon.png", size: "57x57" },
+        { icon: "favicon-196x196.png" },
+        { icon: "favicon-32x32.png" },
+        { icon: "favicon-16x16.png" },
+        { icon: "favicon.png", size: "16x16" },
+        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      ]
+    }
+  end
 end
