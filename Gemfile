401cda98662aed4baae2e73bf33044d69b1c86d8
Andi Staub <sta@panter.ch>
2014-08-02 14:59:50 +0200

    Add optimmization gems.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'middleman-livereload'
 gem 'middleman-sprockets'
 gem 'middleman-gh-pages'
 gem 'middleman-favicon-maker', '~> 3.7'
+gem 'middleman-minify-html'
+gem 'middleman-imageoptim', '~> 0.1.4'
 
 gem 'bootstrap-sass'
 
--- config.rb
+++ config.rb
@@ -63,10 +63,10 @@ activate :livereload
 # Build-specific configuration
 #
 configure :build do
-
-  # For example, change the Compass output styles for deployment
+  # Enable minifiers
   activate :minify_css
   activate :minify_javascript
+  activate :minify_html
 
   # Enable cache buster
   activate :cache_buster
@@ -74,32 +74,35 @@ configure :build do
   # Use relative URLs
   activate :relative_assets
 
-  # Compress PNGs after build
-  # activate :smusher
-
   # Build with nice urls
   activate :directory_indexes
 
   activate :favicon_maker do |f|
     f.template_dir  = File.join(root, 'source', 'img', 'favicon')
     f.icons = {
-      "_favicon.png" => [
-        { icon: "apple-touch-icon-152x152-precomposed.png" },
-        { icon: "apple-touch-icon-144x144-precomposed.png" },
-        { icon: "apple-touch-icon-120x120-precomposed.png" },
-        { icon: "apple-touch-icon-114x114-precomposed.png" },
-        { icon: "apple-touch-icon-76x76-precomposed.png" },
-        { icon: "apple-touch-icon-72x72-precomposed.png" },
-        { icon: "apple-touch-icon-60x60-precomposed.png" },
-        { icon: "apple-touch-icon-57x57-precomposed.png" },
-        { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
-        { icon: "apple-touch-icon.png", size: "57x57" },
-        { icon: "favicon-196x196.png" },
-        { icon: "favicon-32x32.png" },
-        { icon: "favicon-16x16.png" },
-        { icon: "favicon.png", size: "16x16" },
-        { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+      '_favicon.png' => [
+        { icon: 'apple-touch-icon-152x152-precomposed.png' },
+        { icon: 'apple-touch-icon-144x144-precomposed.png' },
+        { icon: 'apple-touch-icon-120x120-precomposed.png' },
+        { icon: 'apple-touch-icon-114x114-precomposed.png' },
+        { icon: 'apple-touch-icon-76x76-precomposed.png' },
+        { icon: 'apple-touch-icon-72x72-precomposed.png' },
+        { icon: 'apple-touch-icon-60x60-precomposed.png' },
+        { icon: 'apple-touch-icon-57x57-precomposed.png' },
+        { icon: 'apple-touch-icon-precomposed.png', size: '57x57' },
+        { icon: 'apple-touch-icon.png', size: '57x57' },
+        { icon: 'favicon-196x196.png' },
+        { icon: 'favicon-32x32.png' },
+        { icon: 'favicon-16x16.png' },
+        { icon: 'favicon.png', size: '16x16' },
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
       ]
     }
   end
+
+  # Activate image optimisation
+  activate :imageoptim
+
+  # Activate gzip for text files
+  activate :gzip
 end
