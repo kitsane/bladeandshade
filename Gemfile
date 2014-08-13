db7902ccddc0e1a637dbe3424414d9e5c3116f49
Andi Staub <sta@panter.ch>
2014-08-13 20:17:17 +0200

    Update middleman.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,20 @@
 source 'https://rubygems.org'
 
-gem 'middleman'
-gem 'middleman-livereload'
-gem 'middleman-sprockets'
-gem 'middleman-gh-pages'
+gem "middleman", "~>3.3.4"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# Favicon maker, multiple sizes
 gem 'middleman-favicon-maker', '~> 3.7'
-gem 'middleman-minify-html'
+
+# Image optimizations without quality loss
 gem 'middleman-imageoptim', '~> 0.1.4'
 
-gem 'bootstrap-sass'
+gem 'middleman-minify-html'
+#
+# Github pages deployment
+gem 'middleman-gh-pages'
 
-gem 'builder'
+gem 'haml'
+gem 'bootstrap-sass'
--- config.rb
+++ config.rb
@@ -1,12 +1,9 @@
+require 'haml'
+require 'bootstrap-sass'
+
 ###
 # Compass
 ###
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-require 'bootstrap-sass'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -20,20 +17,19 @@ require 'bootstrap-sass'
 # Per-page layout changes:
 #
 # With no layout
-# page '/path/to/file.html', :layout => false
+# page "/path/to/file.html", :layout => false
 #
 # With alternative layout
-# page '/path/to/file.html', :layout => :otherlayout
+# page "/path/to/file.html", :layout => :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
-#   page '/admin/*'
+#   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page '/this-page-has-no-template.html', :proxy => '/template-file.html' do
-#   @which_fake_page = 'Rendering a fake page with a variable'
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -42,67 +38,60 @@ require 'bootstrap-sass'
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     'Helping'
+#     "Helping"
 #   end
 # end
 
 set :css_dir, 'css'
-
 set :js_dir, 'js'
-
 set :images_dir, 'img'
 
-# set :relative_links, true
-
-# Enable live reload while working locally
-activate :livereload
-
 # Build-specific configuration
-#
 configure :build do
-  # Enable minifiers
+  # For example, change the Compass output style for deployment
   activate :minify_css
+
+  # Minify Javascript on build
   activate :minify_javascript
+
+  # Minify Html on build
   activate :minify_html
 
   # Enable cache buster
-  activate :cache_buster
-
-  # Use relative URLs
-  activate :relative_assets
+  activate :asset_hash
 
   # Build with nice urls
   activate :directory_indexes
 
+  # Use relative URLs
+  activate :relative_assets
+
+  # Favicon generator
   activate :favicon_maker do |f|
     f.template_dir  = File.join(root, 'source', 'img', 'favicon')
     f.icons = {
       '_favicon.png' => [
         { icon: 'apple-touch-icon-152x152-precomposed.png' },
-        { icon: 'apple-touch-icon-144x144-precomposed.png' },
-        { icon: 'apple-touch-icon-120x120-precomposed.png' },
-        { icon: 'apple-touch-icon-114x114-precomposed.png' },
-        { icon: 'apple-touch-icon-76x76-precomposed.png' },
-        { icon: 'apple-touch-icon-72x72-precomposed.png' },
-        { icon: 'apple-touch-icon-60x60-precomposed.png' },
-        { icon: 'apple-touch-icon-57x57-precomposed.png' },
         { icon: 'apple-touch-icon-precomposed.png', size: '57x57' },
-        { icon: 'apple-touch-icon.png', size: '57x57' },
-        { icon: 'favicon-196x196.png' },
-        { icon: 'favicon-32x32.png' },
-        { icon: 'favicon-16x16.png' },
-        { icon: 'favicon.png', size: '16x16' },
         { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
       ]
     }
   end
 
-  # Activate image optimisation
+  # Image optimisation
   activate :imageoptim
 
   # Activate gzip for text files
   activate :gzip
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
 end
